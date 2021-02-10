6fd5603fe70a20bb2bddb491d55b4fef9f5c247b
Dave Vronay <davevr+github@gmail.com>
2021-02-09 21:13:30 -0800

    update to test pipeline

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "example2" {
   ami           = "ami-047a51fa27710816e"
   instance_type = "t2.micro"
 }
